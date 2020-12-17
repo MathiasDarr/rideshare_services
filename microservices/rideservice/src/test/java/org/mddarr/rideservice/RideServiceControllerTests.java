23890a97c1ac307e06a029657b047efc385e4d3b
mddarr <mddarr@gmail.com>
2020-12-16 17:03:55 -0800

    I'm going to start implmenting tests for the rideservice

---
+++ microservices/rideservice/src/test/java/org/mddarr/rideservice/RideServiceControllerTests.java
@@ -0,0 +1,19 @@
+package org.mddarr.rideservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class RideServiceControllerTests {
+
+    @Test
+    public void contextLoads(){
+        
+    }
+
+}
---
+++ microservices/spring_notes.md
@@ -0,0 +1,8 @@
+
+### Spring Notes ###
+
+@SpringBootApplication -> convenience annotation that adds all of the following
+    - @Configuration 
+    - @EnableAutoConfiguration -> tells spring boot to start adding beans based on classpath settings,
+    - @EnableWebMvc -> Flags the application as a web application and activates key behaviors such as setting up a DispatcherServlet
+    
