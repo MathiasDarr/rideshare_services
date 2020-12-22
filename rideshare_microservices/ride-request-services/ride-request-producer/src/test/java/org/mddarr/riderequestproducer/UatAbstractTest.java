00d576c9ba0dd4c81085b534bc435f327c9cdeff
mddarr <mddarr@gmail.com>
2020-12-22 03:32:33 -0800

    The embeddedKafka test compiles...

--- rideshare_microservices/ride-request-services/ride-request-producer/src/test/java/org/mddarr/riderequestproducer/UatAbstractTest.java
+++ rideshare_microservices/ride-request-services/ride-request-producer/src/test/java/org/mddarr/riderequestproducer/UatAbstractTest.java
@@ -2,13 +2,18 @@ package org.mddarr.riderequestproducer;
 
 
 import org.apache.kafka.clients.consumer.Consumer;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mddarr.riderequests.AvroRideRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(classes = {RideRequestProducerApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@EmbeddedKafka
 public abstract class UatAbstractTest {
 //    @Autowired
 //    private KafkaProperties kafkaProperties;
