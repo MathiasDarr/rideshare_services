83fc354334825df9191c66e7dfd262ef3baed9e3
mddarr <mddarr@gmail.com>
2020-12-24 01:47:14 -0800

    I have a demonstration of word count function.

---
+++ rideshare_microservices/ride-services/ride-receiver/src/main/java/org/mddarr/ridereceiver/RideReceiverApplication.java
@@ -0,0 +1,66 @@
+package org.mddarr.ridereceiver;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.*;
+import org.mddarr.ridereceiver.models.WordCount;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@SpringBootApplication
+public class RideReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideReceiverApplication.class, args);
+	}
+
+	public static final String INPUT_TOPIC = "input";
+	public static final String OUTPUT_TOPIC = "output";
+	public static final int WINDOW_SIZE_MS = 30_000;
+
+
+//	@Bean
+//	public java.util.function.Consumer<KStream<String, String>> process() {
+//
+//		return input ->
+//				input.foreach((key, value) -> {
+//					System.out.println("Key: " + key + " Value: " + value);
+//				});
+//	}
+	@Bean
+	public Function<KStream<Bytes, String>, KStream<Bytes, WordCount>>  process() {
+		return (userClicksStream) -> {
+			userClicksStream.foreach((key, value) -> System.out.println("THE KEY IS AND THE VLAUE IS " + key + " " + value));
+			return 	userClicksStream.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+					.map((key, value) -> new KeyValue<>(value, value))
+					.groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+					.windowedBy(TimeWindows.of(Duration.ofMillis(WINDOW_SIZE_MS)))
+					.count(Materialized.as("WordCounts-1"))
+					.toStream()
+					.map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))));
+		};
+	}
+
+//	@Bean
+//	public Function<KStream<Bytes, String>, KStream<Bytes, WordCount>> process() {
+//
+//		return incoming_words -> incoming_words
+//				.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+//				.map((key, value) -> new KeyValue<>(value, value))
+//				.groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+//				.windowedBy(TimeWindows.of(Duration.ofMillis(WINDOW_SIZE_MS)))
+//				.count(Materialized.as("WordCounts-1"))
+//				.toStream()
+//				.map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))));
+//	}
+
+}
